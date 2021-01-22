166ca65a1ce8d6122cc1d21283a1fa1b0a07fb67
H37kouya <h37kouya@gmail.com>
2021-01-23 00:47:06 +0900

    plugins追加

--- .zshrc
+++ .zshrc
@@ -1,5 +1,8 @@
 SCRIPT_DIR=$HOME/dotfiles
 
+HISTSIZE=100000
+SAVEHIST=100000
+
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
@@ -21,6 +24,7 @@ autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
 ### End of Zinit's installer chunk
 
+source $SCRIPT_DIR/zsh/alias.zsh
 source $SCRIPT_DIR/zsh/plugins.zsh
 source $SCRIPT_DIR/zsh/config.zsh
 
--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,1 +1,22 @@
 zinit ice depth=1; zinit light romkatv/powerlevel10k
+
+zinit ice from"gh-r" as"program"
+zinit load junegunn/fzf-bin
+
+# 入力中のコマンドと一致するコマンドを履歴から探す
+zinit ice wait'2' lucid
+zinit light zsh-users/zsh-autosuggestions
+
+# Fast Syntax Highlighting
+zinit light zdharma/fast-syntax-highlighting
+
+# Ctrl+r でコマンド履歴を検索
+zinit light zdharma/history-search-multi-word
+
+# 移動強化系プラグイン
+ENHANCD_COMPLETION_BEHAVIOR=list
+zinit light b4b4r07/enhancd
+
+# shの補完機能をさらに強化するプラグイン
+zinit ice wait'2' lucid atload"zicompinit; zicdreplay" blockf for \
+  zsh-users/zsh-completions
