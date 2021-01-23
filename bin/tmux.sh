0608aa31370c84fd328fd5f1ff14d85266853044
H37kouya <h37kouya@gmail.com>
2021-01-23 12:03:35 +0900

    feat command改良

--- bin/tmux.sh
+++ bin/tmux.sh
@@ -3,10 +3,16 @@
 
 # set tmux panes
 if [ "$#" -eq 0 ]; then
-  tmux split-window -v
-  tmux split-window -h
-  tmux resize-pane -D 15
-  tmux select-pane -t 1
+    tmux split-window -h
+    tmux split-window -v
+    tmux resize-pane -D 15
+    tmux select-pane -t 1
+    tmux split-window -v
+    tmux select-pane -t 1
+    tmux select-pane -L
+    tmux split-window -v
+    tmux resize-pane -D 5
+    tmux select-pane -U
 else
   case $1 in
     1)
