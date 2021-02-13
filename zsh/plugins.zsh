18d5b71db4ed6538171080a5930acb6ec70fa3ac
H37kouya <h37kouya@gmail.com>
2021-02-13 10:02:31 +0900

    fix delay timing

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -4,19 +4,19 @@ zinit ice from"gh-r" as"program"
 zinit load junegunn/fzf-bin
 
 # 入力中のコマンドと一致するコマンドを履歴から探す
-zinit ice wait'2' lucid
 zinit light zsh-users/zsh-autosuggestions
-zinit ice wait'2' lucid
 zinit light zsh-users/zsh-history-substring-search
 
 # Fast Syntax Highlighting
 zinit light zdharma/fast-syntax-highlighting
 
 # Ctrl+r でコマンド履歴を検索
+zinit ice wait'2' lucid
 zinit light zdharma/history-search-multi-word
 
 # 移動強化系プラグイン
 ENHANCD_COMPLETION_BEHAVIOR=list
+zinit ice wait'2' lucid
 zinit light b4b4r07/enhancd
 
 # shの補完機能をさらに強化するプラグイン
